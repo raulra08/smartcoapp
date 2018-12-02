234aee1f256f6ee5135d5347be847d4e492f6f82
Raul <noreply@noemail.com>
2018-12-02 17:15:27 +0000

    add support to MS App Center

--- app/src/main/java/com/smartco/app/MainActivity.kt
+++ app/src/main/java/com/smartco/app/MainActivity.kt
@@ -8,11 +8,17 @@ import android.widget.Button
 import com.smartco.app.utils.Constants.EXTRA_MESSAGE_KEY
 import com.smartco.app.views.stock.ShopStockActivity
 
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAppCenter()
     }
 
     /** Called when the user taps the Send button */
@@ -23,5 +29,10 @@ class MainActivity : AppCompatActivity() {
             putExtra(EXTRA_MESSAGE_KEY, message)
         }
         startActivity(intent)
+    }
+
+    private fun initAppCenter() {
+        AppCenter.start(application, "1dadff7b-ba5f-4dfb-ac02-b63531b8f7cd",
+                Analytics::class.java, Crashes::class.java)
     }
 }
