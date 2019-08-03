eea01c46c3e5296f5026293527b170100a048e42
Raul <noreply@noemail.com>
2019-08-04 00:30:49 +0100

    (test): added Hamcrest to the project

--- app/src/test/java/com/smartco/app/ExampleUnitTest.kt
+++ app/src/test/java/com/smartco/app/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.smartco.app
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
-
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test
 class ExampleUnitTest {
     @Test
     fun `testing that junit5 is my test framework`() {
-        assertEquals(4, 2 + 2)
+
+        assertThat(4, equalTo(2 + 2))
     }
 }
