2e39635a792e93a02cfc847e020fbe74597a4484
Raul <noreply@noemail.com>
2019-08-08 00:23:21 +0100

    (test): example espresso test and dependencies needed

--- app/src/androidTest/java/com/smartco/app/SelectAndScanInventoryTest.kt
+++ app/src/androidTest/java/com/smartco/app/SelectAndScanInventoryTest.kt
@@ -7,11 +7,15 @@ import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
 import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(AndroidJUnit4::class)
 @LargeTest
 class SelectAndScanInventoryTest {
 
